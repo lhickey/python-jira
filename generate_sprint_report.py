f3cb86954c41cbc6b9b2de4709b491750a15c625
Lisa <lisa@barracuda.(none)>
2014-07-20 17:31:19 +0100

    amalgamated jira queries into one python file and write these to spreadsheet

--- generate_sprint_report.py
+++ generate_sprint_report.py
@@ -1,13 +1,9 @@
+
 import getopt
 import sys
 import json
 import os.path
 
-# import bug_breakdown_for_sprint
-# import write_file
-# import stories_qa_passed_in_sprint
-
-sprint = "Sprint 37"
 
 
 def main(argv):
@@ -18,35 +14,6 @@ def main(argv):
     print configuration.host
     print configuration.project_name
     print configuration.sprint
-
-    if True:
-        return
-
-        # print "Bugs opened in sprint: {}".format(bug_breakdown_for_sprint.bugs_opened_in_sprint())
-        #
-        # print "Bugs fixed in sprint: {}".format(bug_breakdown_for_sprint.bugs_opened_in_sprint())
-        #
-        # print "Blocker bugs: {}".format(bug_breakdown_for_sprint.blocker_bugs_in_sprint())
-        #
-        # print "Critical bugs: {}".format(bug_breakdown_for_sprint.critical_bugs_in_sprint())
-        #
-        # print "Major bugs: {}".format(bug_breakdown_for_sprint.major_bugs_in_sprint())
-        #
-        # print "Minor bugs: {}".format(bug_breakdown_for_sprint.minor_bugs_in_sprint())
-        #
-        # print "Trivial bugs: {}".format(bug_breakdown_for_sprint.trivial_bugs_in_sprint())
-        #
-        # print "Generating list of open bugs in whole project -  ...."
-        #
-        # write_file
-        #
-        # print "Finished generating list of open bugs"
-        #
-        # print "Generating list of stories passed by QA during sprint ..."
-        #
-        # stories_qa_passed_in_sprint
-        #
-        # print "Finished generating list of stories QA Passed"
 
 
 def printUsage():
--- write_file.py
+++ write_file.py
@@ -1,24 +1,54 @@
 from xlutils.copy import copy # http://pypi.python.org/pypi/xlutils
 from xlrd import open_workbook # http://pypi.python.org/pypi/xlrd
-from xlwt import easyxf # http://pypi.python.org/pypi/xlwt
 import open_bugs_in_project
+import report_data
 
-START_ROW = 3 # 0 based (subtract 1 from excel row number)
-col_key = 0
-col_summary = 1
-col_priority = 2
-col_severity = 3
-col_status = 4
-col_affects_version = 5
-col_fix_version = 6
 
 wb = copy(open_workbook('test.xlsx'))
-w_sheet = wb.get_sheet(1)
 
-results = open_bugs_in_project.open_bugs_list()
+def write_bug_count_in_sprint():
+    w_sheet = wb.get_sheet(0)
 
-for index, result in enumerate(results):
-    for column_index in range(col_key, col_fix_version):
-        w_sheet.write(index + START_ROW, column_index, result[column_index])
+    w_sheet.write(0, 0, "Blocker")
+    w_sheet.write(1, 0, "Critical")
+    w_sheet.write(2, 0, "Major")
+    w_sheet.write(3, 0, "Minor")
+    w_sheet.write(4, 0, "Trivial")
+    w_sheet.write(0, 1, report_data.blocker_bugs_in_sprint())
+    w_sheet.write(1, 1, report_data.critical_bugs_in_sprint())
+    w_sheet.write(2, 1, report_data.major_bugs_in_sprint())
+    w_sheet.write(3, 1, report_data.minor_bugs_in_sprint())
+    w_sheet.write(4, 1, report_data.trivial_bugs_in_sprint())
 
-wb.save('test.xlsx')
+    w_sheet.write(6, 0, "Bugs Opened in Sprint")
+    w_sheet.write(6, 1, report_data.bugs_opened_in_sprint())
+    w_sheet.write(7, 0, "Bugs Fixed in Sprint")
+    w_sheet.write(7, 1, report_data.bugs_fixed_in_sprint())
+
+    wb.save('test.xlsx')
+
+
+def write_stories_passed_by_qa():
+
+    w_sheet = wb.get_sheet(1)
+
+    results = open_bugs_in_project.open_bugs_list()
+
+    for index, result in enumerate(results):
+        for column_index in range(0, 2):
+            w_sheet.write(index, column_index, result[column_index])
+
+    wb.save('test.xlsx')
+
+
+def write_open_bugs_in_project():
+
+    w_sheet = wb.get_sheet(2)
+
+    results = report_data.open_bugs_list()
+
+    for index, result in enumerate(results):
+        for column_index in range(0, 6):
+            w_sheet.write(index, column_index, result[column_index])
+
+    wb.save('test.xlsx')
